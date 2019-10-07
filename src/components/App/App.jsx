550322824c64e697404527dd65d8f5c3204e68ba
elenems <mickhaelyarish@gmail.com>
2019-10-07 10:03:12 +0300

    Updated initial structure

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,11 +1,7 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
 function App() {
   return (
     <div className="App">
-  
     </div>
   );
 }
